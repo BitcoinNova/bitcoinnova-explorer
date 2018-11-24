16d4d45a0bbada78ad2c3b981ace2f9d1c6d1941
BitcoinNovaProject <44268242+BitcoinNovaProject@users.noreply.github.com>
2018-11-24 17:38:51 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-        'api' => 'http://127.0.0.1:45223', // API_json_RPC
+        'api' => 'http://explorer.bitcoinnova.org:5454', // API_json_RPC
         'blockTargetInterval' => 120,
         'coinUnits' => 1000000
 );
