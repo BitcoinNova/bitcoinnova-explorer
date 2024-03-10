ac044a41ddaa235777dc43a83c7735d0725da952
SuperBlockchain Pool <55582476+SuperBlockchain-Pool@users.noreply.github.com>
2024-03-10 17:59:28 +0100

    Update Github Pages

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://81.169.166.154:45225';
+var api = 'https://superblockchain.con-ip.com/api/btn/';
 var donationAddress = "EAsX15ieXY1NAk9Yu3NoVBfBqUgsFPsv47Ff4W1t491vHiD8fyGHD7nR7gVk1FrcbP2d2mJfBt3M45NgbV6ZRcSdHixcSPH";
 var blockTargetInterval = 120;
 var coinUnits = 1000000;
